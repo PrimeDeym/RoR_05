125f2c815b329db7307fe929c1b1a34684fe36a5
PrimeDeym <primedeym@gmail.com>
2016-02-18 18:13:10 +0200

    mod Developer.rb

--- Personal/Developer.rb
+++ Personal/Developer.rb
@@ -1,17 +1,44 @@
 require 'pry'
 
 class Developer
+  #нужно доработать метод с константой
   MAX_TASKS = 10
   def initialize(name)
     @name = name
-		
+		@tasks = []
   end
 	
   def add_task(task)
-    puts %Q("#{@name}: добавлена задача "#{task}". Всего в списке задач: ")
-		
+    @tasks.push(task)
+    if @tasks.count == MAX_TASKS
+      raise(ArgumentError, "Слишком много работы!")
+    else
+      puts %Q("#{@name}: добавлена задача "#{task}". Всего в списке задач: #{@tasks.count}")
+    end
+  end
+
+  def tasks
+    @tasks.each_with_index.each { |a, i| puts "#{i+1}. #{a.capitalize}"}
+  end
+
+  def work!
+    done = @tasks.shift
+    if done == nil
+      raise(ArgumentError, "Нечего делать!")
+    else
+      puts %Q("#{@name}: выполнена задача "#{done}". Осталось задач: #{@tasks.count}")
+    end
+  end
+
+  def status
+    case @tasks.count
+    when 0 then puts "свободен"
+    when 1..9 then puts "работаю"
+    when 10 then puts "занят"
+    end
   end
 end
 
 d = Developer.new('Test_Dev')
+
 binding.pry
