4574b79c7a0e5ec9c572883b42cb4a33565f4dd9
PrimeDeym <primedeym@gmail.com>
2016-02-18 15:59:02 +0200

    Deleted hw1 and add Developer.rb

---
+++ Personal/Developer.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+class Developer
+		MAX_TASKS = 10
+	def initialize(name)
+		@name = name
+		
+	end
+	
+	def add_task(task)
+		puts %Q("#{@name}: добавлена задача "#{task}". Всего в списке задач: ")
+		
+	end
+end
+
+d = Developer.new('Test_Dev')
+
+binding.pry
