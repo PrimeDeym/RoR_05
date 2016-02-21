45c970ec7514960ca344b694bc327e575e6c6f88
PrimeDeym <primedeym@gmail.com>
2016-02-21 12:17:13 +0200

    update code

--- Personal/SeniorDeveloper.rb
+++ Personal/SeniorDeveloper.rb
@@ -5,10 +5,9 @@ class SeniorDeveloper < Developer
   MAX_TASKS = 15
 
   def work!
-    not_working
-    if @tasks.size == 1
+    if @tasks.size <= 1
       super
-    elsif rand > 0.5
+    elsif rand > 0.4
       puts %Q("%s: выполнена задача "%s и %s". Осталось задач: %i") %
              [@name, @tasks.shift, @tasks.shift, @tasks.count]
     else
--- main.rb
+++ main.rb
@@ -3,11 +3,4 @@ require_relative 'Personal/Developer'
 require_relative 'Personal/JuniorDeveloper'
 require_relative 'Personal/SeniorDeveloper'
 
-
-
-d = Developer.new('Developer')
-j = JuniorDeveloper.new('Junior')
-s = SeniorDeveloper.new('Senior')
-
-
-binding.pry
+# binding.pry
